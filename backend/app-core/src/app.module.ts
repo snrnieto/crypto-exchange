53ec4811231b3f40589d8cf7c14a8dde866e3230
Sebastian Nieto <snrnieto1999@hotmail.com>
2024-07-11 18:29:09 -0500

    [ADD] added nest modules

--- backend/app-core/src/app.module.ts
+++ backend/app-core/src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    ConfigModule.forRoot(),
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          name: 'global',
+          ttl: parseInt(process.env.RATE_LIMIT_TTL, 10),
+          limit: parseInt(process.env.RATE_LIMIT, 10),
+        },
+      ],
+    }),
+    MongooseModule.forRoot(process.env.DB_URI),
+  ],
   providers: [AppService],
 })
 export class AppModule {}
